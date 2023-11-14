10a5307a5a9514fac0946108bb4ab1f2b555e233
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2023-11-14 16:18:44 -0500

    incomplete kata, ran out of time, have to complete later

---
+++ main.js
@@ -0,0 +1,31 @@
+/**
+ This time no story, no theory. The examples below show you how to write function accum:
+**/
+
+//P.R.E.P
+
+//Parameter - a string will be given
+
+//return to the console
+
+//exmaple - accum("abcd") -> "A-Bb-Ccc-Dddd
+
+//Pseudo code
+
+function accum(s) {
+
+    //seperate the string?
+    let divided=s.split('')
+
+    for (let i = 0; i <= divided.length; i++){
+        let newArray= []
+        
+    }
+	// some for loop that counts
+    
+    //does +1 of the string
+
+    
+}
+
+console.log(accum("abcd"))
